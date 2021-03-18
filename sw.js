d854564300d16fd3ec4c8c62cbdf4db5cfae7783
opocenskyTreyton <54596181+opocenskyTreyton@users.noreply.github.com>
2021-03-18 13:40:42 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,6 +16,9 @@ const PRECACHE_URLS = [
   'images/hello-icon-256.png',
   'images/hello-icon-512.png',
   'images/Cardinal.png',
+  'images/Chicken.png',
+  'images/Meadowlark.png',
+  'images/Mockingbird.png',
   'sw.js'
   ];
 
